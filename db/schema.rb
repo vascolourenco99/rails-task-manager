11e3221735a4438553486dcab1d083437c5459e9
vascocorreia <vascocorreia99@gmail.com>
2022-11-15 12:18:56 +0000

    change db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_120609) do
-  create_table "taks", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_121508) do
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.string "completed"
-    t.string "false"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
